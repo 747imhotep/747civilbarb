818929eb08f9d9189e27e4dc55b81f0e68f6f42d
747imhotep <support@lunyns.com>
2026-04-20 00:18:05 +0200

    Update sender-init.js
    add the exact js code from sender

--- live/js/sender-init.js
+++ live/js/sender-init.js
@@ -6,24 +6,20 @@
 //::::::::::::::::::::::::::::::::::
 
 
-(function (s, e, n, d, er) {
-  s['Sender'] = er;
-  s[er] = s[er] || function () {
-    (s[er].q = s[er].q || []).push(arguments)
-  }, s[er].l = 1 * new Date();
 
-  s[er].on = function(event, callback) {
-    (s[er].listeners = s[er].listeners || {});
-    (s[er].listeners[event] = s[er].listeners[event] || []).push(callback);
-  };
-
-  // LOAD Sender script
-  if (!e.querySelector('script[src*="sender.js"]')) { // Prevent duplicate load
-  const script = e.createElement(n);
-  script.async = true;
-  script.src = d + "/js/v2/sender.js"; // official CDN path
-
-  const firstScript = e.getElementsByTagName(n)[0];
-  firstScript.parentNode.insertBefore(script, firstScript);
-
-})(window, document, 'script', 'https://cdn.sender.net', 'sender');
+  (function (s, e, n, d, er) {
+    s['Sender'] = er;
+    s[er] = s[er] || function () {
+      (s[er].q = s[er].q || []).push(arguments)
+    }, s[er].l = 1 * new Date();
+    s[er].on = function(event, callback) {
+      s[er].listeners = s[er].listeners || {};
+      (s[er].listeners[event] = s[er].listeners[event] || []).push(callback);
+    };
+    var a = e.createElement(n),
+        m = e.getElementsByTagName(n)[0];
+    a.async = 1;
+    a.src = d;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+  sender('8ff476a5273454')
