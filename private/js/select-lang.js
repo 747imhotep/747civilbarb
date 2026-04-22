75e2ad510ba4b3660d77fd603e5c492ee59ba5eb
747imhotep <support@lunyns.com>
2026-04-22 02:50:07 +0200

    Translate home page

--- private/js/select-lang.js
+++ private/js/select-lang.js
@@ -8,6 +8,15 @@
   const el = document.getElementById("lang-text");
 
   setInterval(() => {
-    index = (index + 1) % texts.length;
-    el.textContent = texts[index];
+    // fade out
+    el.classList.add("fade-out");
+
+    setTimeout(() => {
+      // switch text while invisible
+      index = (index + 1) % texts.length;
+      el.textContent = texts[index];
+
+      // fade back in
+      el.classList.remove("fade-out");
+    }, 500); // match CSS transition time
   }, 2000);
