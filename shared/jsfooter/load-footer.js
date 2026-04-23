92b679469fe1057daa103e38823c00601abef630
747imhotep <support@lunyns.com>
2026-04-24 01:14:55 +0200

    Update load-footer.js

--- shared/jsfooter/load-footer.js
+++ shared/jsfooter/load-footer.js
@@ -3,8 +3,15 @@
 //:::::::::::::::::::
 
 
-fetch('/shared/footer/footer.html')
+const lang = document.documentElement.lang || 'en';
+
+const footerFile =
+  lang === 'fr'
+    ? '/shared/footer/footer-fr.html'
+    : '/shared/footer/footer-en.html';
+
+fetch(footerFile)
   .then(res => res.text())
   .then(html => {
-    document.querySelector('footer-placeholder').innerHTML = html;
+    document.getElementById('footer-placeholder').innerHTML = html;
   });
