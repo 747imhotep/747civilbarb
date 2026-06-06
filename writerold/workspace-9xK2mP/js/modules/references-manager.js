76b877956dd0390c8d62e87f78e0ae95026e7dd5
747imhotep <support@lunyns.com>
2026-06-07 00:55:03 +0200

    update storage-folder/
    improve link to storage folder

--- writerold/workspace-9xK2mP/js/modules/references-manager.js
+++ writerold/workspace-9xK2mP/js/modules/references-manager.js
@@ -13,7 +13,7 @@ import { getDraftById, updateLocalDraftStatus } from './data.js';
 import { getRefColorClass } from './utils.js';
 
 // Configuration
-const STORAGE_BASE = 'workspace-9xK2mP/data/storage-folder/'; // Change to your actual path
+import { STORAGE_FOLDER_PATH } from './config.js'; // Change to your actual path
 const REFERENCES_FILE = `${STORAGE_BASE}/references.json`;
 
 // In-memory cache
