16c16295b8729e89de9ca42b2118aa742c86b105
747imhotep <support@lunyns.com>
2026-06-23 15:27:25 +0200

    Copy privat/ Folder in 747civilbarb/

---
+++ private/js/select-lang.js
@@ -0,0 +1,27 @@
+//::::::::::::::::::::::::::::::::::::::
+// Language selection text animation
+//::::::::::::::::::::::::::::::::::::::
+
+
+
+  const texts = [
+    "Select your language",
+    "Sélectionnez votre langue"
+  ];
+
+  let index = 0;
+  const el = document.getElementById("lang-text");
+
+  setInterval(() => {
+    // fade out
+    el.classList.add("fade-out");
+
+    setTimeout(() => {
+      // switch text while invisible
+      index = (index + 1) % texts.length;
+      el.textContent = texts[index];
+
+      // fade back in
+      el.classList.remove("fade-out");
+    }, 500); // match CSS transition time
+  }, 2000);
